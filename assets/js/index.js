4ca54d34c6d399032d382445d197ecdd34168016
zqy <a469224660@qq.com>
2020-09-11 19:36:23 +0800

    修改bug

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,74 +1,77 @@
+
 $(function () {
-    //调用用户得基本信息
+    // 调用 getUserInfo 获取用户基本信息
     getUserInfo()
 
     var layer = layui.layer
-    //点击按钮，实现退出功能
+
+    // 点击按钮，实现退出功能
     $('#btnLogout').on('click', function () {
-        //提示用户是否确认退出
+        // 提示用户是否确认退出
         layer.confirm('确定退出登录?', { icon: 3, title: '提示' }, function (index) {
             //do something
-
-            //1.清空本地存储中的token
+            // 1. 清空本地存储中的 token
             localStorage.removeItem('token')
-            //2.重新跳转到登录页面
+            // 2. 重新跳转到登录页面
             location.href = '/login.html'
 
-            //3.关闭confirm询问框
-            layer.close(index);
-        });
+            // 关闭 confirm 询问框
+            layer.close(index)
+        })
     })
 })
 
-
-
-//获取用户基本信息
+// 获取用户的基本信息
 function getUserInfo() {
     $.ajax({
-        method: 'get',
+        method: 'GET',
         url: '/my/userinfo',
-        //headers是请求头的配置对象
+        // headers 就是请求头配置对象
         // headers: {
-        //     Authorization: localStorage.getItem('token') || ""
+        //   Authorization: localStorage.getItem('token') || ''
         // },
         success: function (res) {
-            // console.log(res);
+            // console.log(res)
             if (res.status !== 0) {
                 return layui.layer.msg('获取用户信息失败！')
             }
-            // 调用 renderAvatara 渲染用户头像
+            // 调用 renderAvatar 渲染用户的头像
             renderAvatar(res.data)
         },
-
-        //不论成功还是失败，都会调用complete回调函数
+        // 不论成功还是失败，最终都会调用 complete 回调函数
         // complete: function (res) {
-        //     //在complete回调函数中，可以使用res.responseJSON拿到服务器响应回来的数据
-        //     if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
-        //         //强制清空本地存储中的token
-        //         localStorage.removeItem('token')
-        //         //强制跳转到登录页面
-        //         location.href = '/login.html'
-        //     }
+        //   // console.log('执行了 complete 回调：')
+        //   // console.log(res)
+        //   // 在 complete 回调函数中，可以使用 res.responseJSON 拿到服务器响应回来的数据
+        //   if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        //     // 1. 强制清空 token
+        //     localStorage.removeItem('token')
+        //     // 2. 强制跳转到登录页面
+        //     location.href = '/login.html'
+        //   }
         // }
     })
 }
 
-
-//渲染用户的头像
+// 渲染用户的头像
 function renderAvatar(user) {
-    // 1.获取用户的名称
+    // 1. 获取用户的名称
     var name = user.nickname || user.username
-    //2.渲染欢迎的文本内容
+    // 2. 设置欢迎的文本
     $('#welcome').html('欢迎&nbsp;&nbsp;' + name)
-    //3.按需求渲染用户的头像
+    // 3. 按需渲染用户的头像
     if (user.user_pic !== null) {
-        //3.1 渲染图片头像
-        $('.layui-nav-img').attr('src', user.user_pic).show()
+        // 3.1 渲染图片头像
+        $('.layui-nav-img')
+            .attr('src', user.user_pic)
+            .show()
         $('.text-avatar').hide()
     } else {
-        //3.2渲染文本头像
+        // 3.2 渲染文本头像
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
-        $('.text-avatar').html(first).show()
+        $('.text-avatar')
+            .html(first)
+            .show()
     }
 }
--- assets/js/user/user_info.js
+++ assets/js/user/user_info.js
@@ -1,46 +1,47 @@
-$(function () {
 
-    // 获取 form 对象
+$(function () {
     var form = layui.form
-    var layer = layui.layer
-    // 自定义校验规则
+
     form.verify({
         nickname: function (value) {
-            if (value.trim().length > 6) {
-                return '昵称长度必须在 1 ~ 6 个字符之间'
+            if (value.length > 6) {
+                return '昵称长度必须在 1 ~ 6 个字符之间！'
             }
         }
     })
 
-    //初始化用户的基本管理信息
+    initUserInfo()
+
+    // 初始化用户的基本信息
     function initUserInfo() {
         $.ajax({
-            method: 'get',
+            method: 'GET',
             url: '/my/userinfo',
             success: function (res) {
                 if (res.status !== 0) {
-                    return layer.msg('获取用户信息失败!')
+                    return layer.msg('获取用户信息失败！')
                 }
-                // console.log(res);
-
-                // 将数据填充到表单中
+                // console.log(res)
+                // 调用 `form.val()` 方法为表单赋值
                 form.val('formUserInfo', res.data)
             }
         })
     }
 
-
-    //重置按钮
+    // 重置表单的数据
     $('#btnReset').on('click', function (e) {
+        // 阻止表单的默认重置行为
         e.preventDefault()
         initUserInfo()
     })
 
-    //监听表单的提交事件
+    // 监听表单的提交事件
     $('.layui-form').on('submit', function (e) {
+        // 阻止表单的默认提交行为
         e.preventDefault()
+        // 发起 ajax 数据请求
         $.ajax({
-            method: 'post',
+            method: 'POST',
             url: '/my/userinfo',
             data: $(this).serialize(),
             success: function (res) {
@@ -48,6 +49,7 @@ $(function () {
                     return layer.msg('更新用户信息失败！')
                 }
                 layer.msg('更新用户信息成功！')
+                // 调用父页面中的方法，重新渲染用户的头像和用户的信息
                 window.parent.getUserInfo()
             }
         })
