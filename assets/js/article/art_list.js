8f33f22912aa43845d6a04b63750bab5466c652a
zqy <a469224660@qq.com>
2020-09-15 15:45:38 +0800

    完善了文章管理的功能 并修改了bug

--- assets/js/article/art_list.js
+++ assets/js/article/art_list.js
@@ -1,10 +1,12 @@
+
 $(function () {
-
     var layer = layui.layer
+    var form = layui.form
+    var laypage = layui.laypage
 
-    //定义美化时间的过滤器
-    template.defaults.imports.dateFormat = function (date) {
-        var dt = new Date(date)
+    // 定义美化时间的过滤器
+    template.defaults.imports.dataFormat = function (date) {
+        const dt = new Date(date)
 
         var y = dt.getFullYear()
         var m = padZero(dt.getMonth() + 1)
@@ -17,33 +19,36 @@ $(function () {
         return y + '-' + m + '-' + d + ' ' + hh + ':' + mm + ':' + ss
     }
 
-    //定义补零的函数
+    // 定义补零的函数
     function padZero(n) {
         return n > 9 ? n : '0' + n
     }
 
-    //定义一个查询的参数对象，将来请求数据的时候，需要将请求参数对象提交到服务器
+    // 定义一个查询的参数对象，将来请求数据的时候，
+    // 需要将请求参数对象提交到服务器
     var q = {
-        pagenum: 1,
-        pagesize: 2,
-        cate_id: '',
-        state: ''
+        pagenum: 1, // 页码值，默认请求第一页的数据
+        pagesize: 2, // 每页显示几条数据，默认每页显示2条
+        cate_id: '', // 文章分类的 Id
+        state: '' // 文章的发布状态
     }
 
     initTable()
     initCate()
 
-    //获取文章列表数据的方法
+    var flag = false
+    // 获取文章列表数据的方法
     function initTable() {
         $.ajax({
-            method: "get",
+            method: 'GET',
             url: '/my/article/list',
             data: q,
             success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg('获取文章列表失败！')
                 }
-                var htmlStr = template('tel-table', res)
+                // 使用模板引擎渲染页面的数据
+                var htmlStr = template('tpl-table', res)
                 $('tbody').html(htmlStr)
 
                 // 调用渲染分页的方法
@@ -52,17 +57,19 @@ $(function () {
         })
     }
 
-    //初始化文章分类的方法
+    // 初始化文章分类的方法
     function initCate() {
         $.ajax({
-            method: 'get',
+            method: 'GET',
             url: '/my/article/cates',
             success: function (res) {
                 if (res.status !== 0) {
                     return layer.msg('获取分类数据失败！')
                 }
+                // 调用模板引擎渲染分类的可选项
                 var htmlStr = template('tpl-cate', res)
                 $('[name=cate_id]').html(htmlStr)
+                // 通过 layui 重新渲染表单区域的UI结构
                 form.render()
             }
         })
@@ -70,11 +77,14 @@ $(function () {
 
     // 为筛选表单绑定 submit 事件
     $('#form-search').on('submit', function (e) {
-        e.preventDefalut()
+        e.preventDefault()
+        // 获取表单中选中项的值
         var cate_id = $('[name=cate_id]').val()
         var state = $('[name=state]').val()
+        // 为查询参数对象 q 中对应的属性赋值
         q.cate_id = cate_id
         q.state = state
+        // 根据最新的筛选条件，重新渲染表格的数据
         initTable()
     })
 
@@ -148,4 +158,11 @@ $(function () {
         })
     })
 
-})
+    // 通过代理的形式，为删除按钮绑定点击事件处理函数
+    $('tbody').on('click', '.btn-edit', function () {
+        // 获取到文章的 id
+        var id = $(this).attr('data-id')
+        location.href = '/article/art_edit.html?id=' + id
+    })
+
+});
--- assets/js/index.js
+++ assets/js/index.js
@@ -62,16 +62,12 @@ function renderAvatar(user) {
     // 3. 按需渲染用户的头像
     if (user.user_pic !== null) {
         // 3.1 渲染图片头像
-        $('.layui-nav-img')
-            .attr('src', user.user_pic)
-            .show()
+        $('.layui-nav-img').attr('src', user.user_pic).show()
         $('.text-avatar').hide()
     } else {
         // 3.2 渲染文本头像
         $('.layui-nav-img').hide()
         var first = name[0].toUpperCase()
-        $('.text-avatar')
-            .html(first)
-            .show()
+        $('.text-avatar').html(first).show()
     }
 }
